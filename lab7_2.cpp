e8a395b495daf748c40321734cdcd8a63f5340f0
Sipurit <gun0820303553@gmail.com>
2024-12-03 16:03:50 +0700

    #

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -8,16 +8,17 @@ int main()
     string date;
     string movie;
     string quote;
-    int id;
+    string id;
     cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
     cout << "?????: ";
-    cin >> name;
+    getline(cin,name);
     cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
     cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
     cout << name << ": ";
     cin >> id;
+    int gearid = (id[0] - '0')*10 +(id[1] - '0') - 12;
     cin.ignore();
-    cout << "Fahsai: I think you may be GEAR " << id-120000000 << ". I have a free movie ticket for you.\n";
+    cout << "Fahsai: I think you may be GEAR " << gearid << ". I have a free movie ticket for you.\n";
     cout << "Fahsai: Let's go to the cinema together!!!\n";
     cout << "Fahsai: What movie do you want to watch?\n";
     cout << name << ": ";
@@ -25,8 +26,8 @@ int main()
     cout << "Fahsai: So....which day are you free to go with me?\n";
     cout << name << ": ";
     getline(cin,date);
-    cout << "Fahsai: Next Monday....that is OK!!! I'm looking forward to watching " << movie << " with you." << endl;
+    cout << "Fahsai: " << date << "....that is OK!!! I'm looking forward to watching " << movie << " with you." << endl;
     cout << name << ": ";
     getline(cin,quote);
-    cout << "Fahsai: 555+ see you " << date << ". Bye Bye \(^ ^)/";
+    cout << "Fahsai: 555+ see you " << date << ". Bye Bye \\(^ ^)/";
 }
